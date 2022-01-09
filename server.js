ac342c8226826cc375c17e9b1168d0ad003d6471
shivang <shivangmathur101@gmail.com>
2022-01-09 18:47:30 +0530

    added tour document

--- server.js
+++ server.js
@@ -21,6 +21,38 @@ mongoose
     console.log('DB connection successful');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR 💥:', 'err');
+  });
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App Running on port ${port}.....`);
