21d3caef0ffaac1c5447e67cf1683242ef14d5ab
shivang <shivangmathur101@gmail.com>
2022-01-22 17:31:38 +0530

    hidded the created at field

--- models/tourModel.js
+++ models/tourModel.js
@@ -54,7 +54,8 @@ const tourSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now()
+    default: Date.now(),
+    select: false
   },
   startDates: [Date]
 });
