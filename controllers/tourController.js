b6b2f0416b2bf73e811c86ce32f5d0d39d2cbdc8
shivang <shivangmathur101@gmail.com>
2022-01-31 15:31:25 +0530

    added new route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
@@ -14,19 +21,14 @@ exports.getAllTours = async (req, res) => {
     //1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
     console.log(JSON.parse(queryStr));
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .lte(25)
-    //   .where('difficulty')
-    //   .equals('medium');
 
     //2) Sorting
     let query = Tour.find(JSON.parse(queryStr));
 
     if (req.query.sort) {
+      console.log(req.query.sort);
+
       const sortBy = req.query.sort.split(',').join(' ');
       console.log(sortBy);
 
@@ -51,15 +53,14 @@ exports.getAllTours = async (req, res) => {
     const skip = (page - 1) * limit;
 
     // console.log(page, limit);
-    query = Tour.find()
-      .skip(skip)
-      .limit(limit);
+    query = query.skip(skip).limit(limit);
 
     if (req.query.page) {
       const numTours = await Tour.countDocuments();
 
       if (skip >= numTours) throw new Error('This page does not exist');
     }
+
     // Execute Query
     const tours = await query;
 
